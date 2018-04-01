3ae507531d81ba8b3c410cd0b5d8130ad80acf23
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2018-03-31 23:43:16 -0700

    Add user routes to router

--- server/router/router.go
+++ server/router/router.go
@@ -1,16 +1,20 @@
 package router
 
 import (
+	"streaming_app/server/config"
 	"streaming_app/server/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Create() *gin.Engine {
+	config := config.Read()
 	router := gin.Default()
 
-	router.GET("/streamers", controllers.GetStreamers)
-	router.GET("/streamers/:id", controllers.GetStreamerById)
+	api := router.Group("/api/" + config.Api.Version)
+
+	api.GET("/users", controllers.GetUserByName)
+	api.GET("/streams/:id", controllers.GetLiveStreamByUser)
 
 	return router
 }
